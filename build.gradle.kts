bd71c197b393b093a8672361814d16375e1648d2
Michael Bull <mikebull94@gmail.com>
2021-02-05 16:44:15 +0000

    Remove bintray & release plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,13 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val SourceSet.kotlin: SourceDirectorySet
-    get() = withConvention(KotlinSourceSet::class) { kotlin }
-
-fun BintrayExtension.pkg(configure: BintrayExtension.PackageConfig.() -> Unit) {
-    pkg(delegateClosureOf(configure))
-}
 
 plugins {
     `java-library`
     `maven-publish`
     kotlin("jvm") version "1.4.30"
     id("com.github.ben-manes.versions") version "0.36.0"
-    id("com.jfrog.bintray") version "1.8.5"
-    id("net.researchgate.release") version "2.8.1"
     id("org.jetbrains.dokka") version "1.4.20"
     id("org.jetbrains.kotlin.plugin.spring") version "1.4.30"
 
@@ -36,7 +24,6 @@ the<DependencyManagementExtension>().apply {
 
 repositories {
     mavenCentral()
-    maven(url = "https://dl.bintray.com/michaelbull/maven")
 }
 
 dependencies {
@@ -98,35 +85,4 @@ publishing {
             artifact(sourcesJar.get())
         }
     }
-}
-
-val bintrayUser: String? by project
-val bintrayKey: String? by project
-
-bintray {
-    user = bintrayUser
-    key = bintrayKey
-    setPublications("mavenJava")
-
-    pkg {
-        repo = "maven"
-        name = project.name
-        desc = project.description
-        websiteUrl = "https://github.com/michaelbull/spring-boot-starter-recaptcha"
-        issueTrackerUrl = "https://github.com/michaelbull/spring-boot-starter-recaptcha-retry/issues"
-        vcsUrl = "git@github.com:michaelbull/spring-boot-starter-recaptcha.git"
-        githubRepo = "michaelbull/spring-boot-starter-recaptcha"
-        setLicenses("ISC")
-    }
-}
-
-val bintrayUpload by tasks.existing(BintrayUploadTask::class) {
-    dependsOn("build")
-    dependsOn("generatePomFileForMavenJavaPublication")
-    dependsOn(sourcesJar)
-    dependsOn(javadocJar)
-}
-
-tasks.named("afterReleaseBuild") {
-    dependsOn(bintrayUpload)
 }
